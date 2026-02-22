e878e1bb75cb898396fd73d0178d764c164f3431
Dipakvishwakarma7777 <vishwakarmadipak480@gmail.com>
2026-02-23 01:32:38 +0530

    "second changes"

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
+require("dotenv").config()
 const express = require("express");
 const app = require("./src/app");
 const mongoose = require("mongoose");
--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,9 @@
 const mongoose = require("mongoose");
 
 function connectToDb() {
-  mongoose
-    .connect(
-      "mongodb+srv://dipak:0d1mzxph9dhwoxCp@cluster0.9lusik1.mongodb.net/app",
-    )
-    .then(() => {
-      console.log("connected to Db");
-    });
+  mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("connected to Db");
+  });
 }
 
 module.exports = connectToDb;
